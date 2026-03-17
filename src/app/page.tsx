76e5a6ac8376963c704194d7fa19252ef732a4ce
anonyfinger <illa999511@gmail.com>
2026-03-17 22:33:08 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
               fill
               priority
               fetchPriority="high"
-              quality={60}
+              quality={50}
               sizes="(max-width: 768px) 480px, (max-width: 1200px) 100vw, 1200px"
               decoding="async"
               className="hero__bg-img"
